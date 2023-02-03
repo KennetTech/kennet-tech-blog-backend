fd0bcf6ef8fa91924a65722c28f54216a1ec67d6
KennetTech <64315533+KennetTech@users.noreply.github.com>
2023-02-03 17:14:23 +0100

    Local mongo server added and GET article function

--- src/server.js
+++ src/server.js
@@ -1,9 +1,52 @@
 import express from "express";
+import { MongoClient } from "mongodb";
+
 
 const app = express();
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
-    res.send('Hello');
+app.get(`/api/articles/:name`, async (req, res) => {
+    const { name } = req.params;
+
+    const client = new MongoClient('mongodb://127.0.0.1:27017/');
+    await client.connect();
+
+    const db = client.db('react-blog-db');
+    
+    const article = await db.collection('articles').findOne({ name });
+
+    if (article) {
+        res.json(article);
+    } else {
+        res.sendStatus(404);
+    }
+    
+});
+
+app.put(`/api/articles/:name/upvote`, (req, res) => {
+    const name = req.params.name
+    const article = articlesInfo.find(a => a.name === name);
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes!!`)
+    } else {
+        res.send('That article doen\'t exist');
+    }
+});
+
+app.post(`/api/articles/:name/comments`, (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text} = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
+    } else {
+        res.send('That article doen\'t exist');
+    }
+    
 });
 
 app.listen(8000, () => {
