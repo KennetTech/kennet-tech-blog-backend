67d3c4a6e6465917b6621c4a26d31cb05d5bd9ef
KennetTech <64315533+KennetTech@users.noreply.github.com>
2023-02-03 20:30:01 +0100

    db connection refactored

--- src/server.js
+++ src/server.js
@@ -1,17 +1,11 @@
 import express from "express";
-import { MongoClient } from "mongodb";
-
+import { db, connectToDb } from './db.js';
 
 const app = express();
 app.use(express.json());
 
 app.get(`/api/articles/:name`, async (req, res) => {
     const { name } = req.params;
-
-    const client = new MongoClient('mongodb://127.0.0.1:27017/');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
     
     const article = await db.collection('articles').findOne({ name });
 
@@ -23,25 +17,32 @@ app.get(`/api/articles/:name`, async (req, res) => {
     
 });
 
-app.put(`/api/articles/:name/upvote`, (req, res) => {
-    const name = req.params.name
-    const article = articlesInfo.find(a => a.name === name);
+app.put(`/api/articles/:name/upvote`, async (req, res) => {
+    const { name } = req.params
+
+    await db.collection('articles').updateOne({ name }, {
+        $inc: {upvotes: 1}, 
+    });
+    
+    const article = await db.collection('articles').findOne({ name });
+
     if (article) {
-        article.upvotes += 1;
         res.send(`The ${name} article now has ${article.upvotes} upvotes!!`)
     } else {
         res.send('That article doen\'t exist');
     }
 });
 
-app.post(`/api/articles/:name/comments`, (req, res) => {
+app.post(`/api/articles/:name/comments`, async (req, res) => {
     const { name } = req.params;
     const { postedBy, text} = req.body;
-
-    const article = articlesInfo.find(a => a.name === name);
+   
+    await db.collection('articles').updateOne({ name }, {
+        $push: { comments: {postedBy, text} },
+    });
+    const article = await db.collection('articles').findOne({ name });
 
     if (article) {
-        article.comments.push({ postedBy, text });
         res.send(article.comments);
     } else {
         res.send('That article doen\'t exist');
@@ -49,6 +50,9 @@ app.post(`/api/articles/:name/comments`, (req, res) => {
     
 });
 
-app.listen(8000, () => {
-    console.log('server is listening on port 8000')
+connectToDb(() => {
+    console.log('succesfully connected to database')
+    app.listen(8000, () => {
+        console.log('server is listening on port 8000')
+    });
 });
